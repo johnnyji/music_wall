660c3984ee3af46efa9b44d368a540ec35fa1895
Johnny Ji <johnny.ji@live.ca>
2015-04-20 11:12:12 -0700

    init commit and migration

---
+++ db/migrate/20150420110632_create_songs.rb
@@ -0,0 +1,13 @@
+class CreateSongs < ActiveRecord::Migration
+  def up
+    create_table :songs do |t|
+      t.string :title
+      t.string :author
+      t.string :url
+      t.timestamps null: false
+    end
+  end
+
+  def down
+  end
+end
